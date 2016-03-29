7531242d7cacbea5dcfbd7c7a8df6620915e055b
Leandro Machado Pereira <llpereiras@gmail.com>
2016-03-29 12:46:07 -0300

    Ajustes para websocket. Adicionando configuração para o ambiente de production

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,36 @@
 //= require moment
 //= require faye
 //= require_tree .
+
+$(function() {
+  var faye = new Faye.Client('http://localhost:9292/faye');
+  faye.subscribe("/messages/new", function(data) {
+    eval(data);
+  });
+});
+
+
+// (function() {
+//   $(document).ready(function() {
+//     var client;
+//     client = new Faye.Client('/faye');
+//     return client.subscribe('/news', function(payload) {
+//       var time;
+//       time = moment(payload.created_at).format('D/M/YYYY H:mm:ss');
+//       $('.count-message').html("" + payload.message);
+//       return window.client = client;
+//     });
+//   });
+//
+// }).call(this);
+
+
+// $(document).ready ->
+//   client = new Faye.Client('/faye')
+//   client.subscribe '/chat', (payload)->
+//     time = moment(payload.created_at).format('D/M/YYYY H:mm:ss')
+//     # You probably want to think seriously about XSS here:
+//     $('.count-message').html("#{payload.message}")
+//     window.client = client
+//
+// # in case anyone wants to play with the inspector.
